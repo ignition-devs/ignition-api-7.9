a682cc8cd3a2cae214d4cb41e543c809dc2cded9
César Román <thecesrom@gmail.com>
2021-11-10 14:44:24 -0800

    build(setup): switch to static metadata
    limit installation to Python 2.7.18 as older versions are no longer
    supported by PyCharm and Visual Studio Code

--- setup.py
+++ setup.py
@@ -1,54 +1,7 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 
 """Ignition API."""
 
-from __future__ import with_statement
+from setuptools import setup
 
-import os
-from codecs import open
-
-from setuptools import find_packages, setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-about = {}
-with open(os.path.join(here, "src", "system", "__version__.py"), "r") as f:
-    exec (f.read(), about)
-
-with open("README.md", "r", "utf-8") as f:
-    readme = str(f.read())
-
-setup(
-    name=about["__title__"],
-    version="{}{}".format(about["__version__"], about["__cycle__"]),
-    description=about["__description__"],
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url=about["__url__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    license=about["__license__"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Manufacturing",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2 :: Only",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Testing :: Mocking",
-    ],
-    keywords="hmi, ignition, inductive automation, scada",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=2.5, <3",
-)
+setup()
