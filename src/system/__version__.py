4eca347a606a1f84ea9cf84a2eebb943e7a35972
César Román <thecesrom@gmail.com>
2022-07-27 13:54:10 -0700

    chore(release): prepare for v7.9.21

--- src/system/__version__.py
+++ src/system/__version__.py
@@ -1,6 +1,6 @@
 """Package information."""
 
-__version__ = "7.9.20"
-__build__ = "2022051210"
+__version__ = "7.9.21"
+__build__ = "2022072613"
 __cycle__ = ""
 __package_version__ = "{0}{1}".format(__version__, __cycle__)
